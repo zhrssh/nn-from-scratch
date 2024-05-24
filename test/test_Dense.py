862498e7aed21db917e71dd9c443bd2e09cdb54b
zhrssh <98146850+zhrssh@users.noreply.github.com>
2024-05-24 17:52:01 +0800

    [test] Tested Dense layer

---
+++ test/test_Dense.py
@@ -0,0 +1,31 @@
+import numpy as np
+from layers.Dense import Dense
+
+
+class TestDense:
+    def activation(self, X):
+        return X
+
+    def test_Dense_init(self):
+        dense = Dense(units=3, activation=self.activation)
+        dense.build(input_shape=(2,))
+
+        assert dense.units == 3
+        assert dense.activation == self.activation
+        assert dense._weights.shape == (2, 3)
+        assert dense._bias.shape == (1, 3)
+
+    def test_Dense_forward(self):
+        dense = Dense(units=3, activation=self.activation)
+        dense.build(input_shape=(10,))
+
+        assert dense._weights.shape == (10, 3)
+        assert dense._bias.shape == (1, 3)
+
+        X = np.random.rand(10)
+        y_pred = dense.forward(X.T)
+
+        assert y_pred.shape == (1, 3)
+        assert y_pred.dtype == np.float64
+
+        print("Predictions:", y_pred)
